c60aaccc37f6530637123ed1cc925c2ee55358b6
Nicholas Levenson <nicklevenson@gmail.com>
2020-11-20 12:28:28 -0800

    commit tree 10226cbb7c524e5d276bbf4b8076320f5d71b4c9

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  attr_accessor :full_name
+  has_many :characters
+  has_many :shows
+
+  def full_name
+   self.full_name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect {|character|"#{character.name} - #{character.show.name}"}
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.collect{|character| character.actor.full_name}
+  end
+
+ 
 end
